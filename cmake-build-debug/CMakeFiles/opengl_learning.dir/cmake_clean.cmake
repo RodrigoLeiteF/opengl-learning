136acd7e999ae5cc8491d25c1c8eda7a6cbcd755
Rodrigo Leite <rodrigo@leite.dev>
2019-11-18 21:22:04 -0300

    please help me immediately

---
+++ cmake-build-debug/CMakeFiles/opengl_learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_learning.dir/src/main.cpp.o"
+  "opengl_learning"
+  "opengl_learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
